00e88facb31ac003ffde3e0eb9ea4353b2fc9cf4
류진호 <dbwlsgh57@naver.com>
2024-04-23 12:52:35 +0900

    #7 - feat : [FE] 홈 컴포넌트 생성

---
+++ frontend/Load_Frontend/lib/home.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('홈 페이지'), // 앱바 타이틀
+      ),
+      body: Center(
+        child: Text('홈 설명 페이지'), // 본문에 표시될 텍스트
+      ),
+    );
+  }
+}
