c2402e0b75d11e6c2ca5f587acc0f96304e0c336
류진호 <dbwlsgh57@naver.com>
2024-04-23 13:14:12 +0900

    #7 - feat : [FE] 404 페이지 추가

---
+++ frontend/Load_Frontend/lib/not_found_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('404 Not Found'),
+      ),
+      body: Center(
+        child: Text('Page not found!'),
+      ),
+    );
+  }
+}
