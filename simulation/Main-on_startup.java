773659de75743fd954a2e2dd5d6ab40793c69b9d
shinyeji28 <xntm10@naver.com>
2024-04-24 22:12:29 +0900

    #12 - feat : [SIMUL] source로 DB Agent 실어 보내기

--- simulation/Main-on_startup.java
+++ simulation/Main-on_startup.java
@@ -1,7 +1,13 @@
-try {
-GoodsImpl goodsService = GoodsImpl.getGoodsDao();
-List<GoodsDto> goodsDto = goodsService.getGoods();
-    System.out.println(goodsDto.toString());
-        } catch(SQLException e) {
-        e.printStackTrace();
-  }
+List<GoodsDto> allGoods = getAllGoods();
+    for (GoodsDto goods : allGoods) {
+Box box = new Box();
+box.id = goods.getId();
+box.width = goods.getWidth();
+box.length = goods.getLength();
+box.height = goods.getHeight();
+box.weight = goods.getWeight();
+box.userId = goods.getUserId();
+box.areaId = goods.getAreaId();
+
+		boxCollection.add(box);
+	}
