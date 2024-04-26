32bc29885cfe548c87c69bdf946737a9a619b324
김보경 <rapsparrow99@naver.com>
2024-04-27 04:10:34 +0900

    #20 - feat : [FE] 상품리스트 페이지 레이아웃 추가

--- frontend/Load_Frontend/lib/pages/delivery_list.dart
+++ frontend/Load_Frontend/lib/pages/delivery_list.dart
@@ -1,22 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:load_frontend/components/nav_rail.dart';
 
+import 'goodsdummy.dart';
+String toDay = DateFormat('yyyy.MM.dd').format(DateTime.now()); //today나타내기
 class DeliveryListPage extends StatelessWidget {
   const DeliveryListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Row(
+      body: Row(
         children: [
           Flexible(
             child: MyNav(),
           ),
           Flexible(
-              child: Center(
-                child: Text('적재 전 배송리스트'),
-              ))
-        ],
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30, top: 10),
+                    child: Text('님의 $toDay일 Loading List입니다.'),
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.all(16.0),
+                sliver: SliverToBoxAdapter(
+                  child: Center(
+                    child: Container(
+                      decoration: BoxDecoration(border: Border.all()),
+                      child: DataTable(
+                        headingRowColor: MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                          return Colors.blue;
+                        }, // 헤더 행의 배경색
+                      ),
+                      columns: [
+                      DataColumn(label: Text('No')),
+                      DataColumn(label: Text('주소')),
+                      DataColumn(label: Text('구역')),
+                      DataColumn(label: Text('바코드')),
+                      DataColumn(
+                        label: Container(
+                          width: 80, // 텍스트 너비 조정
+                          child: Text('태백규격(cm)', overflow: TextOverflow.ellipsis), // 오버플로우 발생 시 '...'로 대체
+                        ),
+                      ),
+                      DataColumn(
+                        label: Container(
+                          width: 80, // 텍스트 너비 조정
+                          child: Text('상품무게(g)', overflow: TextOverflow.ellipsis), // 오버플로우 발생 시 '...'로 대체
+                        ),
+                      ),
+                      // DataColumn(label: Text('태백규격(cm)')),
+                      // DataColumn(label: Text('상품무게(g)')),
+                      ],
+                      rows: List<DataRow>.generate(goodsList.length, (index) {
+                        final good = goodsList[index];
+                        return DataRow(
+                          cells: [
+                            DataCell(Text('${good.goodsId}')),
+                            DataCell(Text(good.area)),
+                            DataCell(Text('...')),
+                            DataCell(Text('...')),
+                            DataCell(Text(
+                              '${good.height} x ${good.length} x ${good.width}')),
+                            DataCell(Text('${good.weight}g')),
+                          ],
+                        );
+                      }),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),)
+          ],
        ),
     );
   }
