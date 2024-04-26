719ec5ed83aa6e3d072d5d76550dad3eb518fc22
김보경 <rapsparrow99@naver.com>
2024-04-26 15:17:23 +0900

    #9 - feat : [FE] 네비게이션 바 구조 완성 및 라우터 연결

--- frontend/Load_Frontend/lib/components/nav_rail.dart
+++ frontend/Load_Frontend/lib/components/nav_rail.dart
@@ -1,178 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
 
-class CustomNavigationDrawer extends StatelessWidget {
+class SidebarComponent extends StatelessWidget {
   final Function(int) onDestinationSelected;
   final int selectedIndex;
 
-  const CustomNavigationDrawer({
+  const SidebarComponent({super.key,
     required this.onDestinationSelected,
     required this.selectedIndex,
-    super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: const Color(0xFF3AB9FF),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color(0xFF3AB9FF),
-              image: DecorationImage(
-                image: AssetImage('assets/images/logo3.png'),
-              )
-            ),
-            child: Text(
-              'LOAD',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
-          ),
-          Material(
-          color: Colors.transparent, // Material 위젯의 색을 투명하게 설정
-          child: InkWell(
-            onTap: () {
-              onDestinationSelected(0);
-              Navigator.pop(context);
-            },
-            splashColor: Colors.blue,
-            child: ListTile(
-              selected: selectedIndex == 0,
-              contentPadding: EdgeInsets.zero,
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 40, // 이미지의 높이와 ListTile의 높이 조절
-                    child: Image.asset(
-                      'assets/images/nav1.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  const SizedBox(height: 8), // 이미지와 제목 사이 간격 조절
-                  const Text(
-                    '배송 리스트',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                    ), // 제목 폰트 크기 조절
-                  ),
-                ],
-              ),
-            ),
-          ),
-          ),
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {
-                onDestinationSelected(1);
-                Navigator.pop(context);
-              },
-              splashColor: Colors.blue,
-              child: ListTile(
-                selected: selectedIndex == 1,
-                contentPadding: EdgeInsets.zero,
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 40, // 이미지의 높이와 ListTile의 높이 조절
-                      child: Image.asset(
-                        'assets/images/nav2.png',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    const SizedBox(height: 8), // 이미지와 제목 사이 간격 조절
-                    const Text(
-                      '트럭 규격 입력',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ), // 제목 폰트 크기 조절
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Material(
-            color: Colors.transparent, // Material 위젯의 색을 투명하게 설정
-            child: InkWell(
-              onTap: () {
-                onDestinationSelected(2);
-                Navigator.pop(context);
-              },
-              splashColor: Colors.blue,
-              child: ListTile(
-                selected: selectedIndex == 2,
-
-                contentPadding: EdgeInsets.zero,
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 40, // 이미지의 높이와 ListTile의 높이 조절
-                      child: Image.asset(
-                        'assets/images/nav3.png',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    const SizedBox(height: 8), // 이미지와 제목 사이 간격 조절
-                    const Text(
-                      '적재 최적화 시뮬레이션',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ), // 제목 폰트 크기 조절
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Material(
-            color: Colors.transparent, // Material 위젯의 색을 투명하게 설정
-            child: InkWell(
-              onTap: () {
-                onDestinationSelected(3);
-                Navigator.pop(context);
-              },
-              splashColor: Colors.blue,
-              child: ListTile(
-                selected: selectedIndex == 3,
-                contentPadding: EdgeInsets.zero,
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 40, // 이미지의 높이와 ListTile의 높이 조절
-                      child: Image.asset(
-                        'assets/images/nav4.png',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    const SizedBox(height: 8), // 이미지와 제목 사이 간격 조절
-                    const Text(
-                      '배송 구역 확인',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ), // 제목 폰트 크기 조절
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-        ],
+    return SidebarX(
+      controller: SidebarXController(selectedIndex: selectedIndex, extended: true),
+      theme: SidebarXTheme(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: const Color(0xFF3AB9FF),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        hoverColor: const Color(0xFF464667),
+        textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+        selectedTextStyle: const TextStyle(color: Colors.white),
+        hoverTextStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        itemTextPadding: const EdgeInsets.only(left: 30),
+        selectedItemTextPadding: const EdgeInsets.only(left: 30),
+        itemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: const Color(0xFF3AB9FF)),
+        ),
+        selectedItemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color(0xFF464667),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.28),
+              blurRadius: 30,
+            )
+          ],
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+          size: 20,
+        ),
+        selectedIconTheme: const IconThemeData(
+          color: Colors.white,
+          size: 20,
+        ),
       ),
+      extendedTheme: const SidebarXTheme(
+        width: 230,
+        decoration: BoxDecoration(
+          color: Color(0xFF3AB9FF),
+        ),
+      ),
+      footerDivider: const Divider(color: Colors.white, height: 1),
+      headerBuilder: (context, extended) {
+        return SizedBox(
+          height: 130,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/logo3.png'),
+                const SizedBox(height: 8), // Adjust spacing between Image and Text
+                const Text(
+                  'LOAD',
+                  textAlign: TextAlign.center, // Align text to center horizontally
+                  style: TextStyle(
+                    color: Colors.white,
+                    // Define your text style here
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      items: [
+        SidebarXItem(
+          icon: Icons.list_alt,
+          label: '배송리스트',
+          onTap: () {
+            onDestinationSelected(0);
+          },
+        ),
+        SidebarXItem(
+          icon: Icons.local_shipping,
+          label: '트럭 규격 입력',
+          onTap: () {
+            onDestinationSelected(1);
+          },
+        ),
+        SidebarXItem(
+          icon: Icons.view_in_ar,
+          label: '적재 최적화 시뮬레이션',
+          onTap: () {
+            onDestinationSelected(2);
+          },
+        ),
+        SidebarXItem(
+          icon: Icons.location_on,
+          label: '배송구역 확인',
+          onTap: () {
+            onDestinationSelected(3);
+          },
+        ),
+      ],
     );
   }
 }
--- frontend/Load_Frontend/lib/pages/home.dart
+++ frontend/Load_Frontend/lib/pages/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import '../components/nav_rail.dart';
-import '../components/side_nav_bar.dart';
 
 class dummyData{
   String ?type;
@@ -26,75 +24,44 @@ class HomePage extends StatelessWidget {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Logistic Optimize Algorithm Dive'), // 앱바 타이틀
       ),
-      body:
-      Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                const ListTile(title: Text("여긴 그냥 소개하는 페이지에요.")),
-                const ListTile(title: Text("나중에 바뀔겁니다.")),
-                ElevatedButton(
-                  onPressed: (){
-                    Navigator.pushNamed(context, '/sign-in-up');
-                  },
-                  child: const Text('로그인 페이지로 이동'),
-                ),
-                ElevatedButton(
-                  onPressed: (){
+      body: MyNav()
+    );
+  }
+}
+class MyNav extends StatelessWidget {
+  const MyNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.centerLeft,
+        child: SidebarComponent(
+          onDestinationSelected: (index) {
+            switch (index) {
+                  case 0:
                     Navigator.pushNamed(context, '/delivery-list');
-                  },
-                  child: const Text('적재 전 배송리스트 페이지로 이동'),
-                ),
-                ElevatedButton(
-                  onPressed: (){
+                    break;
+                  case 1:
                     Navigator.pushNamed(context, '/set-truck-specifications');
-                  },
-                  child: const Text('트럭 사이즈 설정 페이지로 이동'),
-                ),
-                ElevatedButton(
-                  onPressed: (){
+                    break;
+                  case 2:
                     Navigator.pushNamed(context, '/box-simulation');
-                  },
-                  child: const Text('박스 시뮬레이션 페이지로 이동'),
-                ),
-                ElevatedButton(
-                  onPressed: (){
+                    break;
+                  case 3:
                     Navigator.pushNamed(context, '/delivery-simulation');
-                  },
-                  child: const Text('배송 시뮬레이션 (맵) 페이지로 이동'),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: const CustomNavigationBar(),// 하단 네비게이션 바 import
-      drawer: CustomNavigationDrawer(
-        onDestinationSelected: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushNamed(context, '/delivery-list');
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/set-truck-specifications');
-              break;
-            case 2:
-              Navigator.pushNamed(context, '/box-simulation');
-              break;
-            case 3:
-              Navigator.pushNamed(context, '/delivery-simulation');
-              break;
-          }
-        },
-        selectedIndex: 0,
+                    break;
+                }
+            // Handle selection logic
+            },
+            selectedIndex: 0, // Provide the selected index
+        ),
       ),
     );
   }
