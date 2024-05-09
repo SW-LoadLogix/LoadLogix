552f6ccc0de0fd4a9e92622156a2d6d55d12d5a2
정건준 <dlans321@naver.com>
2024-05-09 21:58:45 +0900

    #60 - refactor : [BE] CarService 수정

--- backend/src/main/java/org/ssafy/load/application/CarService.java
+++ backend/src/main/java/org/ssafy/load/application/CarService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.ssafy.load.common.dto.ErrorCode;
 import org.ssafy.load.common.exception.CommonException;
-import org.ssafy.load.dao.CarRepository;
 import org.ssafy.load.dao.WorkerRepository;
 import org.ssafy.load.domain.CarEntity;
 import org.ssafy.load.domain.WorkerEntity;
@@ -18,19 +17,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CarService {
     private final WorkerRepository workerRepository;
-    private final CarRepository carRepository;
 
     @Transactional
     public CarResponse updateCarSize(CarChangeRequest carChangeRequest, Long workerId) {
-        Optional<WorkerEntity> worker = workerRepository.findById(workerId);
-        if (worker.isEmpty())
-            throw new CommonException(ErrorCode.USER_NOT_FOUND);
+        Optional<WorkerEntity> workerOptional = workerRepository.findById(workerId);
+        WorkerEntity worker = workerOptional.orElseThrow(() -> new CommonException(ErrorCode.USER_NOT_FOUND));
 
-        Optional<CarEntity> car = carRepository.findById(worker.get().getCar().getId());
-        if (car.isEmpty())
+        CarEntity car = worker.getCar();
+        if (car == null)
             throw new CommonException(ErrorCode.CAR_NOT_FOUND);
 
-        car.get().updateCar(carChangeRequest.carHeight(), carChangeRequest.carLength(), carChangeRequest.carWidth());
-        return CarResponse.from(car.get());
+        car.updateCar(carChangeRequest.carHeight(), carChangeRequest.carLength(), carChangeRequest.carWidth());
+        return CarResponse.from(car);
     }
 }
