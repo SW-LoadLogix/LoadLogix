c3ba4be25df804ab2aef21c20c5f9633f15bfb34
김보경 <rapsparrow99@naver.com>
2024-05-11 16:44:59 +0900

    #79 - feat : [FE] map API 문제 fix

---
+++ frontend/Load_Frontend/api_key.js
@@ -0,0 +1,6 @@
+//const GOOGLE_MAPS_API_KEY='AIzaSyB0AIcKFnA0x2YlN7VyQ2NihQg8_S0-ALA';
+
+var googleMapsApiKey = 'AIzaSyB0AIcKFnA0x2YlN7VyQ2NihQg8_S0-ALA';
+var script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}`;
+document.head.appendChild(script);
