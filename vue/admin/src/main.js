1364fd777b9dd6b7d9806cb7d9b6e71be8aacea3
윤정인 <liae100@naver.com>
2024-05-13 22:48:43 +0900

    #78 - feat : [FE] URL 설정, axios 수정

--- vue/admin/src/main.js
+++ vue/admin/src/main.js
@@ -1,13 +1,21 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //새로고침시 데이터 유지를 위한 설정
 
-const appInstance = createApp(App);
-appInstance.use(store);
-appInstance.use(router);
-appInstance.use(ArgonDashboard);
-appInstance.mount("#app");
+const app = createApp(App);
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); //새로고침시 데이터 유지를 위한 설정
+
+app.use(pinia);
+app.use(store);
+app.use(router);
+app.use(ArgonDashboard);
+app.mount("#app");
--- vue/admin/src/utils/adminAxios.js
+++ vue/admin/src/utils/adminAxios.js
@@ -1,34 +1,36 @@
 import axios from "axios";
-import { useAuthStore } from "@/stores/auth";
+import { useAuthStore } from "@/store/auth";
 import router from "../router";
 
-//비동기 요청 전 인터셉트
-axios.interceptors.request.use(
+// 백엔드의 URL과 포트 설정
+const baseURL = process.env.VITE_APP_BASE_URL;
+
+// axios 인스턴스 생성
+const axiosInstance = axios.create({
+  baseURL: baseURL, // 설정한 baseURL 사용
+});
+
+// 비동기 요청 전 인터셉트
+axiosInstance.interceptors.request.use(
   (config) => {
     const authStore = useAuthStore();
     config.headers.Authorization = `Bearer ${authStore.token}`;
-    // console.log("요청 intercept", config);
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-//비동기 요청 응답 수신 시, 인터셉트
-axios.interceptors.response.use(
+// 비동기 요청 응답 수신 시, 인터셉트
+axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    // console.log("응답 에러 : ", error);
-    //401에러 and 토큰 만료의 경우 ( 네트워크 응답 에러의 경우 response가 존재하지 않으므로 체크 필요 )
     if (
       error.response &&
       error.response.data.status === 401 &&
       error.response.data.message === "Token Expired"
     ) {
-      //store의 유저정보 clear
       const authStore = useAuthStore();
       authStore.clearUser();
-
-      //로그인 페이지 이동 및 alert
       router.push("/login");
       alert("인증이 만료되었습니다. 다시 로그인 해주세요.");
     }
@@ -36,4 +38,5 @@ axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-export default axios;
+
+export default axiosInstance;
