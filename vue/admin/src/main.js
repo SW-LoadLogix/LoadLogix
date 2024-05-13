f269248c99841b8961491ae7117fa0e468bfbf3e
윤정인 <liae100@naver.com>
2024-05-13 11:08:14 +0900

    #78 - feat : [FE] api 연결 구현

--- vue/admin/src/main.js
+++ vue/admin/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //새로고침시 데이터 유지를 위한 설정
+
+const app = createApp(App);
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); //새로고침시 데이터 유지를 위한 설정
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
--- vue/admin/vue.config.js
+++ vue/admin/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    port: 8080, // 프론트엔드 개발 서버의 포트
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081', // 백엔드 서버 주소
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' } // '/api' 부분을 URL에서 제거
+      }
+    }
+  }
 })
