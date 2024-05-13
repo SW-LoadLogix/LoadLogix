cb74a119ae340be64c89633d1548f7fb487b81ea
윤정인 <liae100@naver.com>
2024-05-13 21:27:17 +0900

    #78 - feat : [FE] admin 생성, jwt 토큰 처리, api 연결

--- vue/admin/src/main.js
+++ vue/admin/src/main.js
@@ -1,16 +1,13 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
 import App from "./App.vue";
+import store from "./store";
 import router from "./router";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //새로고침시 데이터 유지를 위한 설정
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import ArgonDashboard from "./argon-dashboard";
 
-const app = createApp(App);
-
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate); //새로고침시 데이터 유지를 위한 설정
-
-app.use(pinia);
-app.use(router);
-
-app.mount("#app");
+const appInstance = createApp(App);
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(ArgonDashboard);
+appInstance.mount("#app");
