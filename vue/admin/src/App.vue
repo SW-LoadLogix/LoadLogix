92b7b113423f89776f868c27aa9848c58d785f3e
윤정인 <liae100@naver.com>
2024-05-13 21:27:17 +0900

    #78 - feat : [FE] admin 생성, jwt 토큰 처리, api 연결

--- vue/admin/src/App.vue
+++ vue/admin/src/App.vue
@@ -1,26 +1,70 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<!--
+=========================================================
+* Vue Argon Dashboard 2 - v4.0.0
+=========================================================
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+* Product Page: https://creative-tim.com/product/vue-argon-dashboard
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import Sidenav from "./examples/Sidenav";
+import Configurator from "@/examples/Configurator.vue";
+import Navbar from "@/examples/Navbars/Navbar.vue";
+import AppFooter from "@/examples/Footer.vue";
+
+const store = useStore();
+const isNavFixed = computed(() => store.state.isNavFixed);
+const darkMode = computed(() => store.state.darkMode);
+const isAbsolute = computed(() => store.state.isAbsolute);
+const showSidenav = computed(() => store.state.showSidenav);
+const layout = computed(() => store.state.layout);
+const showNavbar = computed(() => store.state.showNavbar);
+const showFooter = computed(() => store.state.showFooter);
+const showConfig = computed(() => store.state.showConfig);
+const hideConfigButton = computed(() => store.state.hideConfigButton);
+const toggleConfigurator = () => store.commit("toggleConfigurator");
+
+const navClasses = computed(() => {
+  return {
+    "position-sticky bg-white left-auto top-2 z-index-sticky":
+      isNavFixed.value && !darkMode.value,
+    "position-sticky bg-default left-auto top-2 z-index-sticky":
+      isNavFixed.value && darkMode.value,
+    "position-absolute px-4 mx-0 w-100 z-index-2": isAbsolute.value,
+    "px-0 mx-4": !isAbsolute.value,
+  };
+});
 </script>
+<template>
+  <div
+    v-show="layout === 'landing'"
+    class="landing-bg h-100 bg-gradient-primary position-fixed w-100"
+  ></div>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <sidenav v-if="showSidenav" />
+
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 border-radius-lg"
+  >
+    <!-- nav -->
+
+    <navbar :class="[navClasses]" v-if="showNavbar" />
+
+    <router-view />
+
+    <app-footer v-show="showFooter" />
+
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
+    />
+  </main>
+</template>
