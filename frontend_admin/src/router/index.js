fc5d28dca5acc28c4edf6b94d8b5837dda1d3f9d
shinyeji28 <xntm10@naver.com>
2024-05-14 16:48:32 +0900

    #89 - refector : [FE] billing 삭제

--- frontend_admin/src/router/index.js
+++ frontend_admin/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Tables from "../views/Tables.vue";
-import Billing from "../views/Billing.vue";
 import Profile from "../views/Profile.vue";
 import Signup from "../views/Signup.vue";
 import Signin from "../views/Signin.vue";
@@ -21,11 +20,6 @@ const routes = [
     path: "/tables",
     name: "Tables",
     component: Tables,
-  },
-  {
-    path: "/billing",
-    name: "Billing",
-    component: Billing,
   },
   {
     path: "/profile",
