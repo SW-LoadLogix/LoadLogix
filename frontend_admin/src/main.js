bc05981c73023955f6c6c7d7774b08cbb8ed75c8
윤정인 <liae100@naver.com>
2024-05-14 10:25:24 +0900

    #90 - feat : [FE] axios intercept 설정

--- frontend_admin/src/main.js
+++ frontend_admin/src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //새로고침시 데이터 유지를 위한 설정
 
 const appInstance = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); //새로고침시 데이터 유지를 위한 설정
+
+appInstance.use(pinia);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
