28ed1f3a0eb9806b4f63f27cda3cd24d0448d31a
shinyeji28 <xntm10@naver.com>
2024-05-16 13:08:26 +0900

    #109 - fix : [BE] admin response 수정

--- backend/src/main/java/org/ssafy/load/dto/response/RackStoreCountResponse.java
+++ backend/src/main/java/org/ssafy/load/dto/response/RackStoreCountResponse.java
@@ -1,8 +1,13 @@
 package org.ssafy.load.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RackStoreCountResponse(
-    int rackLine,
-    long totalCount
+
+        @JsonProperty("rack_line")
+        int rackLine,
+        @JsonProperty("total_count")
+        long totalCount
 ) {
 
 }
