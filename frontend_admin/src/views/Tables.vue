518dd50ef272bc0994b36da0c4bcd4ab2e574486
윤정인 <liae100@naver.com>
2024-05-16 17:49:50 +0900

    #114 - feat: [FE] WorkersTable.vue 생성

--- frontend_admin/src/views/Tables.vue
+++ frontend_admin/src/views/Tables.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthorsTable from "./components/AuthorsTable.vue";
 import ProjectsTable from "./components/ProjectsTable.vue";
+import WorkersTable from "./components/WorkersTable.vue";
 </script>
 <template>
   <div class="py-4 container-fluid">
@@ -12,6 +13,11 @@ import ProjectsTable from "./components/ProjectsTable.vue";
     <div class="mt-4 row">
       <div class="col-12">
         <projects-table />
+      </div>
+    </div>
+    <div class="mt-4 row">
+      <div class="col-12">
+        <workers-table />
       </div>
     </div>
   </div>
