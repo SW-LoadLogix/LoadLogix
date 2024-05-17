abb09c81d53bbe5a740affb8c207e5e284206082
윤정인 <liae100@naver.com>
2024-05-17 15:24:32 +0900

    #122 - feat : [FE] 실시간 입고/출고 리스트 로딩 화면 추가

--- frontend_admin/src/views/components/ProjectsTable.vue
+++ frontend_admin/src/views/components/ProjectsTable.vue
@@ -10,14 +10,16 @@ const formatTimestamp = (timestamp) => {
   return formattedDate;
 };
 
+const isLoading = ref(false);
 const goods = ref([]);
 const getLoadedGoodsRequest = async () => {
+  isLoading.value = true;
   const { data } = await getLoadedGoods();
   goods.value = data.result;
   for (let i = 0; i < goods.value.length; i++) {
     goods.value[i].created_at = formatTimestamp(goods.value[i].created_at);
   }
-  console.log(goods.value);
+  isLoading.value = false;
 };
 
 getLoadedGoodsRequest();
@@ -28,7 +30,47 @@ getLoadedGoodsRequest();
       <h6>실시간 출고</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
-      <div class="table-responsive p-0" style="max-height: 400px; overflow-y: auto;">
+      <div v-if="isLoading" class="table-responsive p-0" style="max-height: 400px; overflow-y: auto;">
+        <table class="table align-items-center mb-0">
+          <!-- 테이블 상단 -->
+          <thead>
+            <tr>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                담당 기사
+              </th>
+              <th
+                class="align-middle text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                구역
+              </th>
+              <th
+                class="align-middle text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+              >
+                주소
+              </th>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >
+                무게
+              </th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                박스 타입
+              </th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                출고 시각
+              </th>
+            </tr>
+          </thead>
+        </table>
+        <div style="display: flex; justify-content: center; align-items: center; height: 50vh;">
+          <div class="spinner-border text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+      <div v-else class="table-responsive p-0" style="max-height: 400px; overflow-y: auto;">
         <table class="table align-items-center mb-0">
           <!-- 테이블 상단 -->
           <thead>
