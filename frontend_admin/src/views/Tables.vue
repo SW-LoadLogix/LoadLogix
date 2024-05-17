408cb23faad48c8a502833455f8c21f44148ebd4
윤정인 <liae100@naver.com>
2024-05-17 12:26:11 +0900

    #122 - feat : [FE] admin 디자인 수정

--- frontend_admin/src/views/Tables.vue
+++ frontend_admin/src/views/Tables.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AuthorsTable from "./components/AuthorsTable.vue";
 import ProjectsTable from "./components/ProjectsTable.vue";
-import WorkersTable from "./components/WorkersTable.vue";
 </script>
 <template>
   <div class="py-4 container-fluid">
@@ -13,11 +12,6 @@ import WorkersTable from "./components/WorkersTable.vue";
     <div class="mt-4 row">
       <div class="col-12">
         <projects-table />
-      </div>
-    </div>
-    <div class="mt-4 row">
-      <div class="col-12">
-        <workers-table />
       </div>
     </div>
   </div>
