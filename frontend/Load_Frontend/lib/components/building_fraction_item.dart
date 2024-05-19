de93b8d04f9a2a51b37d4897db7c6bc500cd554f
김보경 <rapsparrow99@naver.com>
2024-05-19 18:46:01 +0900

    #139 - feat : [FE] 배송 상품 개수 오류 수정

--- frontend/Load_Frontend/lib/components/building_fraction_item.dart
+++ frontend/Load_Frontend/lib/components/building_fraction_item.dart
@@ -22,99 +22,85 @@ class BuildingItem extends StatelessWidget {
   Widget build(BuildContext context) {
     AreaService areaService = AreaService();
     UserStore userStore = Provider.of<UserStore>(context, listen: false);
-    int currIndex = 0;
-    return FutureBuilder<List<BuildingData>>(
-      future: areaService.getBuildingPriority(userStore.token),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          List<BuildingData> buildings = snapshot.data!;
-          int totalGoodsSum = buildings.fold(0, (sum, building) => sum + building.totalGoods);
-          return Column(
-            children: buildings.map((building) {
-              final String apiKey = dotenv.get("GOOGLE_MAPS_API_KEY"); // BuildingData 모델에 apiKey 필드를 추가해야 함
-              final double percentage = (building.totalGoods / totalGoodsSum) * 100;
-              final String url =
-                  "https://maps.googleapis.com/maps/api/streetview"
-                  "?size=400x300"
-                  "&location=${building.latitude},${building.longitude}"
-                  "&fov=$fov"
-                  "&heading=$heading"
-                  "&pitch=$pitch"
-                  "&key=$apiKey";
-              final String fIndexStr = "F${(currIndex++).toString().padLeft(3, '0')}";
-              // print (url);
-              //
-              // Completer<Size> completer = Completer<Size>();
-              //
-              // Image.network(url).image.resolve(ImageConfiguration()).addListener(
-              //   ImageStreamListener((ImageInfo info, bool _) {
-              //     completer.complete(Size(info.image.width.toDouble(), info.image.height.toDouble()));
-              //   }),
-              // );
 
-              return Container(
-                padding: EdgeInsets.all(16),
-                margin: EdgeInsets.only(bottom: 24),
-                decoration: BoxDecoration(
-                  color: primaryLight,
-                  borderRadius: BorderRadius.circular(12),
+    List<BuildingData> deliveryData =
+        Provider.of<DeliveryStore>(context, listen: true).buildingPriority;
+
+    int currIndex = 0;
+    return Column(
+      children: [
+        ...deliveryData.map((building) {
+          final String apiKey = dotenv
+              .get("GOOGLE_MAPS_API_KEY"); // BuildingData 모델에 apiKey 필드를 추가해야 함
+          // final double percentage = (building.totalGoods / totalGoodsSum) * 100;
+          final String url = "https://maps.googleapis.com/maps/api/streetview"
+              "?size=400x300"
+              "&location=${building.latitude},${building.longitude}"
+              "&fov=$fov"
+              "&heading=$heading"
+              "&pitch=$pitch"
+              "&key=$apiKey";
+          final String fIndexStr =
+              "F${(currIndex++).toString().padLeft(3, '0')}";
+
+          return Container(
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.only(bottom: 24),
+            decoration: BoxDecoration(
+              color: primaryLight,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 160,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(url),
+                    ),
+                  ),
                 ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 160,
-                      height: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: Colors.transparent,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(url),
+                SizedBox(width: 14),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        fIndexStr,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 14,
                         ),
                       ),
-                    ),
-                    SizedBox(width: 14),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            fIndexStr,
-                            //"택배개수: ${building.totalGoods.toString()}개",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontStyle: FontStyle.italic,
-                              fontSize: 14,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            building.buildingName,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            "${percentage.toStringAsFixed(2)}%", // 소수점 2자리까지 표시
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
-                          ),
-                        ],
+                      SizedBox(height: 4),
+                      Text(
+                        building.buildingName,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 4),
+                      Text(
+                        "택배개수: ${building.totalGoods.toString()}개",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400, fontSize: 15),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            }).toList(),
+              ],
+            ),
           );
-        }
-      },
+        })
+      ],
     );
   }
 }
